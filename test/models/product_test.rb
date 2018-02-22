327db228f22aca50dd7a213a8555aa5188640f02
Ivan2k <motoface@yandex.ru>
2018-02-22 23:22:19 +0300

    ver

---
+++ test/models/product_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+	fixtures :products
+	
+	test "product attributes must not be empty" do
+	# свойства товара не должны оставаться пустыми
+	product = Product.new
+	assert product.invalid?
+	assert product.errors[:title].any?
+	assert product.errors[:description].any?
+	assert product.errors[:price].any?
+	assert product.errors[:image_url].any?
+	end
+
+	test " product is not valid without a unique title - i18n" do
+		product = Product.new(title:
+		products(:ruby).title,
+		description: "yyy",
+		price:
+		1,
+		image_url:
+		"fred.gif")
+		assert product.invalid?
+
+		assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+		product.errors[:title]
+		end
+end
+	
+	
